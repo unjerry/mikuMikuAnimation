364c6ba80bc2dfb80b1957e8c4c3c41f97429d12
unjerry <2351688512@qq.com>
2024-09-27 20:10:18 +0800

    polscoTest_polyscope_geometryCentral_displaybunny

--- src/polscoTest.cpp
+++ src/polscoTest.cpp
@@ -1,12 +1,22 @@
+#include "geometrycentral/surface/manifold_surface_mesh.h"
+#include "geometrycentral/surface/meshio.h"
+#include "geometrycentral/surface/surface_mesh.h"
+
 #include "polyscope/polyscope.h"
-// #include "polyscope/surface_mesh.h"
+#include "polyscope/surface_mesh.h"
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+// a few options
 int main()
 {
+    std::unique_ptr<SurfaceMesh> mesh;
+    std::unique_ptr<VertexPositionGeometry> geometry;
+    std::tie(mesh, geometry) = readSurfaceMesh("bunny.obj");
+
     polyscope::init(); // initialize the gui
 
-    // // add the mesh to the gui
-    // polyscope::registerSurfaceMesh("my mesh",
-    // 	geometry->vertexPositions, mesh->getFaceVertexList());
+    // add the mesh to the gui
+    polyscope::registerSurfaceMesh("my mesh", geometry->vertexPositions, mesh->getFaceVertexList());
 
     polyscope::show(); // pass control to the gui until the user exits
 }
